f07d2f5fb7da58ede9b67c0b4d4598eac4a0ba8b
Sachin Singh <sachinsingh2002.ssss@gmail.com>
2025-04-06 22:19:51 +0530

    updated

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -3,7 +3,7 @@ const year = new Date().getFullYear();
 export default function Footer() {
   return (
     <footer className="p-5 h-14 bg-background bg-black text-white">
-      Made with Axe {year}
+      {/* Made with Axe {year} */}
     </footer>
   );
 }
